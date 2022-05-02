479fdfef4c7ff2f5db9006e2f80849076182e4be
Adilson Custódio <opc@vml01prd-automantion.sub05011604370.vcnprdacstics.oraclevcn.com>
2022-05-02 00:06:00 +0000

    Adicionado SHEBANG e comentários

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print('Hello, World!')
+#!/usr/bin/env python3 
+#Shebang - é um comentario especial #! que vai enviar albuma informaçao para o SO Linux
+# para comentar #
+# Este programa imprime Hello World
+print('Hello, World!')  # test-ignore (este comentario como se fosse uma tag )
+
 print('adilson'.upper())
