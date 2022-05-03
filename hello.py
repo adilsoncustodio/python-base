15cb4598401ea268f8ba931feafe5b1a6ecb3574
Adilson Custódio <opc@vml01prd-automantion.sub05011604370.vcnprdacstics.oraclevcn.com>
2022-05-03 22:36:37 +0000

    implementação de  multilinguas

--- hello.py
+++ hello.py
@@ -2,6 +2,29 @@
 #Shebang - é um comentario especial #! que vai enviar albuma informaçao para o SO Linux
 # para comentar #
 # Este programa imprime Hello World
-print('Hello, World!')  # test-ignore (este comentario como se fosse uma tag )
+"""
 
-print('adilson'.upper())
+
+
+"""
+
+
+__version__ = "0.0.1"
+__author__ = "Adilson Custodio "
+__license__ = "Unlicense"
+
+
+import os 
+
+current_language = "os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello,  World!"
+
+if current_language == "pt_BR";
+    msg = "Olá, Mundo!"
+elif currente_language == "it_IT"
+    msg = "iCiao,  Mondo"
+
+elif currente_language == "it_IT"
+    msg = "Cao,  Mondo"
+print(msg)
